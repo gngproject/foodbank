7f5bee3ac172cd0867e15c3623905b2c60cb5f8f
alvin2723 <alvinjulian87@gmail.com>
2020-09-11 09:49:33 +0700

    update home

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,6 +1,42 @@
 @extends('layout.app')
 
 @section('content')
-    <h2 class="mt-5 mb-3 text-center">Laravel 5.8 For Beginners!</h2>
-    <p class="text-center">Lorem ipsum dolor sit amet consectetur adipisicing elit Aut.</p>
+<div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+    <ol class="carousel-indicators">
+        <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+        <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+        <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+    </ol>
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+        <img class="d-block w-100" src="https://i.postimg.cc/bNQp0RDW/1.jpg" alt="First slide">
+        <div class="carousel-caption d-none d-md-block">
+            <h5>Banner Text one</h5>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Maxime, nulla, tempore. Deserunt excepturi quas vero.</p>
+        </div>
+    </div>
+    <div class="carousel-item">
+        <img class="d-block w-100" src="https://i.postimg.cc/MKqwz5T1/2.jpg" alt="Second slide">
+        <div class="carousel-caption d-none d-md-block">
+            <h5>Banner Text two</h5>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Maxime, nulla, tempore. Deserunt excepturi quas vero.</p>
+        </div>
+    </div>
+    <div class="carousel-item">
+        <img class="d-block w-100" src="https://i.postimg.cc/0y2F6Gpp/3.jpg" alt="Third slide">
+        <div class="carousel-caption d-none d-md-block">
+            <h5>Banner Text three</h5>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Maxime, nulla, tempore. Deserunt excepturi quas vero.</p>
+        </div>
+    </div>
+  </div>
+    <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+      <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+      <span class="sr-only">Previous</span>
+    </a>
+    <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+      <span class="carousel-control-next-icon" aria-hidden="true"></span>
+      <span class="sr-only">Next</span>
+    </a>
+</div>
 @endsection
--- resources/views/layout/app.blade.php
+++ resources/views/layout/app.blade.php
@@ -4,18 +4,24 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="{{asset('css/app.css')}}"> {{-- <- bootstrap css --}}
-    <title>@yield('title','Laravel 5.8 Basics')</title>
+   {{-- <- bootstrap css --}}
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    
+    <link rel="stylesheet" href="{{asset('css/style.css')}}"> 
+    {{-- <title>@yield('title','Laravel 5.8 Basics')</title> --}}
 </head>
 <body>
     {{-- That's how you write a comment in blade --}}
     
     @include('navbar')
     
-    <main class="container mt-4">
+    <main>
         @yield('content')
     </main>
-    {{-- @include('inc.footer') --}}
+    @include('footer')
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
     <script src="{{asset('js/app.js')}}"></script> {{-- <- bootstrap and jquery --}}
     
 </body>
